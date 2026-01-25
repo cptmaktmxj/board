4b948d500f396e511ff8e57c29fbca75233e44d9
cptmaktmxj <158984411+cptmaktmxj@users.noreply.github.com>
2026-01-26 06:44:17 +0900

    게시판 리스트 상태 추가

--- frontend/src/pages/board/BoardListPage.jsx
+++ frontend/src/pages/board/BoardListPage.jsx
@@ -2,7 +2,9 @@ import { useNavigate } from "react-router-dom";
 import BoardLayout from "../../layout/BoardLayout.jsx";
 
 export default function BoardListPage(){
-    /*현재 선택된 tag랑 board도 상태로 추가*/
+    /*현재 선택된 tag랑 board도 상태로 추가, 또는 data로 분리하는것도 고려*/
+    const tags=[]
+    const boards=[contentbox,qna,info,free,noti]
     const navigate=useNavigate()
 
     function handleSearch(){
