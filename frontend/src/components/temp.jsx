b8945607fc243c6f3f8fc0bb8999f7df115df760
cptmaktmxj <158984411+cptmaktmxj@users.noreply.github.com>
2026-01-30 20:08:40 +0900

    에러메시지 추가

--- frontend/src/components/temp.jsx
+++ frontend/src/components/temp.jsx
@@ -188,7 +188,7 @@ export function Error(){
     return (
         <div className="error-text">
             <h1>404 Not Found</h1>
-            <p>설명</p>
+            <p>잘못된 경로로 접근했거나 페이지를 찾을 수 없습니다.</p>
         </div>       
     )
 }
